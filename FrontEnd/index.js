4243b3e724c6fd2a9226711be0333ddf1300f5aa
Mel0ness <melodic.shyness@gmail.com>
2023-03-03 01:28:01 +0100

    suppression d'éléments / gestion des données en mode éditeur même avec changement de filtres / Deuxième panneau de modale (seulement physique, non foonctionnel) / Vérification du fonctionnement / Prochain objectif => Travail modale deuxième partie / Utilisation d'un cookie pour le mode admin avec session storage si user refuse les cookies (norme européenne)

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -6,6 +6,8 @@ let date = Date.parse(new Date());
 let connectedSecurity = connectedTime + 3600000;
 let connectedAlert = window.localStorage.getItem("connectedAlert");
 const body = document.querySelector("body");
+let newDataBaseFiltre = null;
+let newDataBase = [];
 
 const disconnect = () => {
   window.localStorage.clear();
@@ -15,7 +17,7 @@ const disconnect = () => {
 };
 
 //Modale_______________________________________________________________________________
-//Elements sur le body ne fonctionnent pas au niveau du click????
+
 const modale = () => {
   const modaleFiltre = document.getElementById("modale");
   const modaleDiv = document.getElementById("modale-item");
@@ -30,7 +32,7 @@ const modale = () => {
 
         </div>
         <div class="line"></div>
-        <input type="submit" value="Ajouter une photo">
+        <input type="submit" value="Ajouter une photo" id="ajouter-click">
         <p class="delete">Supprimer la galerie</p>
       </div>
   `;
@@ -39,68 +41,126 @@ const modale = () => {
 
   const galerieDyn = document.getElementById("galerie-dyn");
   if (dataBaseFiltres === null) {
-    createElementsGalerie(dataBase, galerieDyn);
+    createElementsGalerie(newDataBase, galerieDyn);
   } else {
-    createElementsGalerie(dataBaseFiltres, galerieDyn);
+    createElementsGalerie(newDataBaseFiltre, galerieDyn);
   }
+  const cross = document.getElementById("cross");
+  cross.addEventListener("click", () => {
+    fermeture(modaleDiv, modaleFiltre);
+  });
+
+  modaleFiltre.addEventListener("click", () => {
+    fermeture(modaleDiv, modaleFiltre);
+  });
+  const ajout = document.getElementById("ajouter-click");
+  ajout.addEventListener("click", () => {
+    modaleAjout();
+  });
+};
+
+const fermeture = (e, f) => {
+  e.innerHTML = "";
+  f.classList.remove("modale-class_visible");
+  galerie.innerHTML = "";
+  if (newDataBaseFiltre) {
+    createElements(newDataBaseFiltre);
+  } else {
+    createElements(newDataBase);
+  }
+};
+
+const modaleAjout = () => {
+  const modaleFiltre = document.getElementById("modale");
+  const modaleDiv = document.getElementById("modale-item");
+  modaleDiv.innerHTML = `
+  <img src="./assets/icons/Arrow_Back.png" alt="flêche de retour"
+  id="back" class="back">
+<div class="cross" id="cross">
+  <div class="cross-elem cross-left"></div>
+  <div class="cross-elem cross-right"></div>
+</div>
+<div class="modale-ajout">
+  <h3>Ajout photo</h3>
+
+  <form action="#" method="post"></form>
+  <div class="ajout-photo">
+    <div class="image-click"><img src="./assets/icons/mountains.png" alt="mountains"
+        class="mountains"><img src="./assets/icons/sun.png" alt="sun" class="sun"></div>
+    <div class="click-ajout">+ Ajouter photo</div>
+    <div class="indications">jpg, png : 4mo max</div>
+  </div>
+  <label for="titre">Titre</label>
+  <input id="titre" name="titre" type="text">
+  <label for="categorie">Catégorie</label>
+  <select name="categorie" id="categorie">
+    <option value=""></option>
+    <option value="Objets">Objets</option>
+    <option value="Appartements">Appartements</option>
+    <option value="Hotels & restaurants">Hotels & restaurants</option>
+
+  </select>
+  <div class="line"></div>
+  <input type="submit" value="Valider" class="inactive">
+  </form> 
+</div>`;
+
   const cross = document.getElementById("cross");
   cross.addEventListener("click", () => {
     modaleDiv.innerHTML = "";
     modaleFiltre.classList.remove("modale-class_visible");
   });
-  modaleFiltre.addEventListener("click", () => {
+
+  const back = document.getElementById("back");
+  back.addEventListener("click", () => {
     modaleDiv.innerHTML = "";
-    modaleFiltre.classList.remove("modale-class_visible");
+    modale();
   });
 };
-
-// const modale = () => {
-//   body.innerHTML += `<div class="modale-item" id="modale-item">
-//       <div class="cross" id="cross">
-//         <div class="cross-elem cross-left"></div>
-//         <div class="cross-elem cross-right"></div>
-//       </div>
-//       <div class="modale-galerie">
-//         <h3>Galerie photo</h3>
-//         <div class="galerie-dyn" id="galerie-dyn">
-
-//         </div>
-//         <div class="line"></div>
-//         <input type="submit" value="Ajouter une photo">
-//         <p class="delete">Supprimer la galerie</p>
-//       </div>
-//     </div>
-//     <div class="modale" id="modale"></div>`;
-//   const modaleItem = document.getElementById("modale-item");
-//   const modaleFiltre = document.getElementById("modale");
-//   modaleFiltre.classList.add("modale-class_visible");
-//   const galerieDyn = document.getElementById("galerie-dyn");
-//   createElementsGalerie(dataBase, galerieDyn);
-//   const cross = document.getElementById("cross");
-//   cross.addEventListener("click", () => {
-//     deleting(modaleItem, modaleFiltre);
-//   });
-//   modaleFiltre.addEventListener("click", () => {
-//     deleting(modaleItem, modaleFiltre);
-//   });
-// };
-
-// const deleting = (e, f) => {
-//   e.remove();
-//   f.remove();
-// };
 
 const createElementsGalerie = (e, f) => {
   for (let i = 0; i < e.length; i++) {
     const item = e[i];
     const img = document.createElement("div");
     img.classList.add("galerie-dyn_item");
-    img.innerHTML = `<img src=${item.imageUrl} alt=${item.title}>
+    img.id = `id${parseInt(i) + 1}`;
+    img.innerHTML = `<img src=${item.imageUrl} alt=${item.title} class="img-dyn">	<div class="logo-black" id="super${img.id}"><img src="./assets/icons/bin.svg" alt="bin"></div>
     <p>éditer</p>
 `;
 
     f.appendChild(img);
+    const numberOne = document.getElementById("id1");
+    numberOne.innerHTML += `<div class="logo-black2"><img src="./assets/icons/multicross.svg" alt="multicross"></div>`;
   }
+  const galerieDyn = document.getElementById("galerie-dyn");
+
+  const deleteImg = document.querySelectorAll(".logo-black");
+
+  deleteImg.forEach((e) => {
+    e.addEventListener("click", () => {
+      let superId = e.id;
+      let finalId = Number(superId.slice(7)) - 1;
+      console.log(finalId);
+      if (dataBaseFiltres) {
+        newDataBaseFiltre.splice(finalId, 1);
+        f.innerHTML = "";
+        if (dataBaseFiltres[0].category.name == "Objets") {
+          filtresObjets = newDataBaseFiltre;
+        } else if (dataBaseFiltres[0].category.name == "Appartements") {
+          filtresAppartements = newDataBaseFiltre;
+        } else {
+          filtresHotels = newDataBaseFiltre;
+        }
+
+        createElementsGalerie(newDataBaseFiltre, galerieDyn);
+      } else {
+        newDataBase.splice(finalId, 1);
+        f.innerHTML = "";
+        filtresGlobaux = newDataBase;
+        createElementsGalerie(newDataBase, galerieDyn);
+      }
+    });
+  });
 };
 
 //Locale storage_______________________________________________________________________________
@@ -145,28 +205,12 @@ async function fetchAPI() {
   galerie.innerHTML = "";
   const response = await fetch("http://localhost:5678/api/works");
   dataBase = await response.json();
+  newDataBase = Array.from(dataBase);
   console.log(dataBase);
   createElements(dataBase);
 }
 
 fetchAPI();
-
-// Async function but scope probs! Better in module or call function each time? ++ module doesn't accept to define variables??? ________________
-//Choix difficile = > Module est parfait pour avoir notre élément défini dans tout le document alors que le async/await ne le défini que dans un scope précis (Réutilisation des fonctions dans le scope direct!!!)
-// = > non-module permet de définir nos variables dans tous le document et d'utiliser le JS en pleine capacité.
-//Séparation des js modules et classiques?
-
-// const response = await fetch("http://localhost:5678/api/works");
-// dataBase = await response.json();
-
-// console.log(dataBase);
-
-// Some test ________________________________________________________
-
-// console.log(array[7]);
-
-// array.splice(3, 4);
-// console.log(array);
 
 //Création des éléments en dynamique ______________________________________________________
 
@@ -190,22 +234,6 @@ let dataBaseFiltres = null;
 filtres.forEach((filtre) => {
   filtre.addEventListener("click", (e) => {
     filtrer(e.target.id);
-    // switch (e.target.id) {
-    //   case "all":
-    //     filtrer(e.target.id);
-    //     break;
-    //   case "Objets":
-    //     filtrer(e.target.id);
-    //     break;
-    //   case "Appartements":
-    //     filtrer(e.target.id);
-    //     break;
-    //   case "Hotels_&_restaurants":
-    //     filtrer(e.target.id);
-    //     break;
-    //   default:
-    //     null;
-    // }
   });
 });
 
@@ -235,32 +263,26 @@ const filterMinusFunction = (index) => {
   });
 };
 
-// Besoin de recharger une API? Doute dans ce cadre précis...
-
-// const filtresFinaux = (e) => {
-//   let components = e.toString();
-//   components = components.replace("_", " ");
-//   components = components.replace("_", " ");
-
-//   async function fetchAPIFiltre() {
-//     const response = await fetch("http://localhost:5678/api/works");
-//     dataBase = await response.json();
-//     dataBaseFiltres = dataBase.filter((d) => {
-//       return d.category.name == components;
-//     });
-//     galerie.innerHTML = "";
-//     createElements(dataBaseFiltres);
-//   }
-//   fetchAPIFiltre();
-// };
-
 // Sans chargement nouveau d'API
+let filtresGlobaux = null;
 
 const filtresAll = () => {
-  galerie.innerHTML = "";
-  createElements(dataBase);
   dataBaseFiltres = null;
+  newDataBaseFiltre = null;
+
+  if (filtresGlobaux == null) {
+    filtresGlobaux = dataBase;
+    galerie.innerHTML = "";
+    createElements(filtresGlobaux);
+  } else if (filtresGlobaux !== null) {
+    galerie.innerHTML = "";
+    createElements(filtresGlobaux);
+  }
 };
+
+let filtresAppartements = null;
+let filtresObjets = null;
+let filtresHotels = null;
 
 const filtresFinaux = (e) => {
   let components = e.toString();
@@ -271,6 +293,30 @@ const filtresFinaux = (e) => {
   dataBaseFiltres = dataBase.filter((d) => {
     return d.category.name == components;
   });
-  galerie.innerHTML = "";
-  createElements(dataBaseFiltres);
+  newDataBaseFiltre = dataBase.filter((d) => {
+    return d.category.name == components;
+  });
+
+  if (components == "Appartements" && filtresAppartements == null) {
+    filtresAppartements = dataBaseFiltres;
+    galerie.innerHTML = "";
+    createElements(filtresAppartements);
+  } else if (components == "Appartements" && filtresAppartements !== null) {
+    galerie.innerHTML = "";
+    createElements(filtresAppartements);
+  } else if (components == "Objets" && filtresObjets == null) {
+    filtresObjets = dataBaseFiltres;
+    galerie.innerHTML = "";
+    createElements(filtresObjets);
+  } else if (components == "Objets" && filtresObjets !== null) {
+    galerie.innerHTML = "";
+    createElements(filtresObjets);
+  } else if (components == "Hotels & restaurants" && filtresHotels == null) {
+    filtresHotels = dataBaseFiltres;
+    galerie.innerHTML = "";
+    createElements(filtresHotels);
+  } else if (components == "Hotels & restaurants" && filtresHotels !== null) {
+    galerie.innerHTML = "";
+    createElements(filtresHotels);
+  }
 };
